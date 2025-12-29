c44cb1bcb54f927356577c9532598f52620476f2
danilserov <danil.serov2010@yandex.ru>
2025-12-29 13:50:16 +0300

    dapr in admin container

--- mt_admin/DbAdmin/sql_scripts/levels.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.levels (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    zoom_level TEXT NOT NULL,
-    zoom_min INT NOT NULL,
-    zoom_max INT NOT NULL
-);
